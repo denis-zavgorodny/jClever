bc103d951be7665535cf0b65a1e8fb0f7b112c49
Denis Zavgorodny <zavgorodny@alterego.biz.ua>
2012-05-26 20:02:19 +0300

    jClever v 0.01 select only

---
+++ js/jClever.js
@@ -0,0 +1,115 @@
+(function($){
+    $.fn.jClever = function(options) {
+        var options = $.extend(
+                                {
+                                    text: ''
+                                },
+                                options
+                                );
+        var selects = [];                        
+        var methods = {
+                        init: function(element) {
+                            //Инициализируем селекты
+                            var innerCounter = 9999;
+                            var tabindex = 1;
+                            $(element).find('select').each(function(){
+                                methods.selectActivate(this,innerCounter, tabindex);
+                                innerCounter--;
+                                tabindex++;
+                            });
+                        },
+                        destroy: function() {
+                            //
+                            alert('s');
+                        },
+                        selectActivate: function(select, innerCounter, tabindex) {
+
+                            selects[$(select).attr('name')] = {
+                                                                    object: $(select),
+                                                                    updateFromHTML: function(data){
+                                                                                            $('select[name='+this.object[0].name+']').html(data).trigger('update');
+                                                                                            
+                                                                                            return false;
+                                                                                        }
+                                                                };
+
+                            $(select).wrap('<div class="jClever-element"><div class="jClever-element-select-wrapper"><div class="jClever-element-select-wrapper-design"><div class="jClever-element-select-wrapper-design">').after('<span class="jClever-element-select-center"></span><span class="jClever-element-select-right"></span><ul class="jClever-element-select-list" style="z-index:'+innerCounter+';"></ul>');
+                            var selectObject = $(select).parents('.jClever-element').attr('tabindex',tabindex);
+                            var selectText = selectObject.find('.jClever-element-select-center');
+                            var selectRight = selectObject.find('.jClever-element-select-right');
+                            var selectList = selectObject.find('.jClever-element-select-list');
+                            $(select).find('option').each(function(){
+                                selectObject.find('.jClever-element-select-list')
+                                            .append($('<li data-value="'+$(this).val()+'"><span><i>'+$(this).text()+'</i></span></li>'));
+                            });
+                            selectText.text($(select).find('option:eq(0)').text());
+                            selectObject.on('click', '.jClever-element-select-center',function(){
+                                selectList.show();
+                            });
+                            selectObject.on('click', '.jClever-element-select-right' ,function(){
+                                selectList.show();
+                            });
+                            selectList.blur(function(){
+                                $(this).hide();
+                            });
+                            selectObject.on('click','li' ,function(event){
+                                var value = $(this).data('value');
+                                $(select).find('option').removeAttr('selected');
+                                $(select).find('option[value='+value+']').attr('selected','selected');
+                                $(select).trigger('change');
+                                selectList.hide();
+                                return false;
+                            });
+                            $(select).change(function(){
+                                selectText.text($(this).find(':selected').text());
+                            });
+                            $(select).bind('update',function(){
+                                var ul = $(this).parents('.jClever-element-select-wrapper')
+                                        .find('.jClever-element-select-list')
+                                        .empty();
+                                $(this).find('option').each(function(){
+                                    ul.append($('<li data-value="'+$(this).val()+'"><span><i>'+$(this).text()+'</i></span></li>'));
+                                });
+                                $(this).parents('.jClever-element-select-wrapper').find('.jClever-element-select-center').text($(select).find('option:eq(0)').text());    
+                            });
+                            selectObject.focus(function(){$(this).addClass('focused')}).blur(function(){$(this).removeClass('focused')});
+                            // Отслеживаем нажатие клавиш для управления клавиатурой
+                            selectObject.keydown(function(e){
+                                var selectedIndex = $(select)[0].selectedIndex;
+                                switch(e.keyCode){
+                                    case 40: /* Down */
+                                        if (selectedIndex < $(select).find('option').length-1){ selectedIndex++; }
+                                        break;
+                                    case 38: /* Up */
+                                        if (selectedIndex > 0){ selectedIndex-=1; }
+                                        break;
+                                    default:
+                                        return;
+                                        break;
+                                }
+                                $(select)[0].selectedIndex = selectedIndex;
+                                console.log(selectedIndex);
+                                selectObject.find('li.selected').removeClass('selected');
+                                selectObject.find('li:eq('+selectedIndex+')').addClass('selected');
+                                selectObject.find('option').removeAttr('selected');
+                                selectObject.find('option:eq('+selectedIndex+')').attr('selected','selected');
+                                $(select).trigger('change');
+                                //selectText.text(selectObject.find(':selected').text());
+                                return false;
+                            });    
+                        },
+        };
+        var publicApi = {
+                            selectCollection: selects
+                        };
+        this.jClever = publicApi;
+        return this.each(function(){
+            methods.init(this);
+        });
+    };
+    /**************************Вспомогательная секция********************/
+    //Спасибо jNiсe за идею
+        $(document).mousedown(function(event){
+            if ($(event.target).parents('.jClever-element-select-wrapper').length === 0) { $('.jClever-element-select-list:visible').hide(); }
+    });
+})(jQuery);    
